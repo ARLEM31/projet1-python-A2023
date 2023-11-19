c1c55c7f9e922164dc0d8a3cc72ebbf363432306
ARLEM31 <arthur.lemay1@ulaval.ca>
2023-11-19 11:58:01 -0500

    fonction_analyser_commande

--- _pycache_/phase1.py
+++ _pycache_/phase1.py
@@ -2,7 +2,7 @@
 
 
 import argparse, json, requests, datetime
-from datetime import datetime
+from datetime import datetime as dt
 
 
 def analyser_commande():
@@ -15,8 +15,31 @@ def analyser_commande():
         symboles à traiter, et les attributs «début», «fin» et «valeur»
         associés aux arguments optionnels de la ligne de commande.
     """
-    parser = argparse.ArgumentParser()
-
+    parser = argparse.ArgumentParser(
+        description = 'Va chercher les valeurs historiques de symboles boursiers')
+    parser.add_argument('symbole', nargs='+', help = "Nom d'un symbole boursier")
+    parser.add_argument('-f', '--fin', dest = 'fin', metavar='DATE',
+        default = dt.today().date().strftime("%Y-%m-%d",
+        help = 'La date recherchée la plus ancienne (format: AAAA-MM-JJ)'))
+    parser.add_argument('-d', '--début', dest = 'début', metavar= 'DATE',
+        help = 'La date recherchée la plus récente (format: AAAA-MM-JJ)')
+    parser.add_argument('-v', '--valeur',
+        help = "La valeur désirée (par défaut: fermeture)", default= 'fermeture',
+        choices=['fermeture', 'ouverture', 'min', 'max', 'volume'])
 
 
     return parser.parse_args()
+
+
+def produire_historique():
+    '''Produit un historique en fonction du symbole et de la date demandé'''
+
+    url = f'https://pax.ulaval.ca/action/{symbole}/historique/'
+    timeline = {"début":date_debut, "fin": date_fin}
+    reponse = json.loads(requests.get(url, timeline).text)
+    historique = []
+    for keys in reponse["historique"]:
+        historique.append((dt.strptime(keys, "%Y-%m-%d").date() ,
+                            reponse["historique"][keys][info_demande]))
+
+    return historique
